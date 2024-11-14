15731d9883f5045a59ba2982f2dec1a5a4875085
Sjur <sjursaa@gmail.com>
2024-11-14 15:25:58 +0100

    add basic todo object constructor, and test from index js with two tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import "./styles.css";
+import { TodoItem } from "./todo.js";
+
+const task1 = new TodoItem(
+  "mow lawn",
+  "yardwork type thing",
+  "tomorrow",
+  "high",
+);
+const task2 = new TodoItem(
+  "mow lawn2",
+  "again but this time better",
+  "next week",
+  "low",
+);
 
 console.log("hello");
+console.log(task1);
+console.log(task2);
--- src/todo.js
+++ src/todo.js
@@ -1,7 +1,9 @@
 // TODO: add notes, and checklist property to object
-export function todoItem(title, description, dueDate, priority) {
+// TODO: add ability to move todoItem from project to project (project responsibiltiy probz)
+export function TodoItem(title, description, dueDate, priority) {
   this.title = title;
   this.description = description;
   this.dueDate = dueDate;
   this.priority = priority;
+  // return "hello";
 }
