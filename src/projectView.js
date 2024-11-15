b56ce9ab7d3c04ea7ed0e731bd1e40c9a51d5f0f
Sjur <sjursaa@gmail.com>
2024-11-16 00:42:54 +0100

    show contents of project to projectView

--- src/projectView.js
+++ src/projectView.js
@@ -1,16 +1,28 @@
 export function ProjectPage(project) {
   this.project = project;
+
   const page = document.querySelector("#content");
   const h1 = document.createElement("h1");
   h1.innerText = this.project.name;
   const p = document.createElement("p");
-  p.innerText = this.project.listOfTasks;
+
+  let taskList = this.project.listOfTasks;
+  console.log(taskList);
+  console.log("elements dropping");
+  let string;
   page.appendChild(h1);
-  page.appendChild(p);
+  // TODO: Fix so that each item in array is displayed
+  taskList.forEach((element) => {
+    string =
+      element.title + element.description + element.dueDate + element.priority;
 
-  const name = this.project.name;
-
-  // page.appendChild(name);
+    p.innerText = string;
+    page.appendChild(p);
+    console.log(element.title);
+    console.log(element.description);
+    console.log(element.dueDate);
+    console.log(element.priority);
+  });
 
   console.log("appending stuff");
   console.log(this.project.name);
